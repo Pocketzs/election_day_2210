c88acacd75d4c16640f96f549ed3ffc90eb70dbc
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-01 10:20:02 -0500

    test: Add vote_for! test

--- spec/candidate_spec.rb
+++ spec/candidate_spec.rb
@@ -26,4 +26,18 @@ describe Candidate do
       expect(diana.votes).to eq 0
     end
   end
+
+  describe '#vote_for!' do
+    it 'increases the number of votes by 1' do
+      diana = Candidate.new({name: "Diana D", party: :democrat})
+
+      diana.vote_for!
+
+      expect(diana.votes).to eq 1
+
+      diana.vote_for!
+
+      expect(diana.votes).to eq 2
+    end
+  end
 end
