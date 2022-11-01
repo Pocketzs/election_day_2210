49c54ac89db9c472ee161824501482df5fd675f1
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-01 10:37:36 -0500

    feat: Pass register_candidate! tests

--- lib/race.rb
+++ lib/race.rb
@@ -6,4 +6,10 @@ class Race
     @office = office
     @candidates = []
   end
+
+  def register_candidate!(attributes)
+    new_can = Candidate.new(attributes)
+    @candidates << new_can
+    new_can
+  end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -32,5 +32,13 @@ describe Race do
       expect(candidate1.name).to eq "Diana D"
       expect(candidate1.party).to eq :democrat
     end
+
+    it 'stores new candidates in candidates array' do
+      race = Race.new("Texas Governor")
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+
+      expect(race.candidates).to eq [candidate1, candidate2]
+    end
   end
 end
