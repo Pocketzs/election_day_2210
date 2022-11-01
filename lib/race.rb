23e08ae1b1664216569989973b243ce79dc9abf8
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-01 11:55:19 -0500

    feat: Begin iteration 4 up to winner method

--- lib/race.rb
+++ lib/race.rb
@@ -5,11 +5,25 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(attributes)
     new_can = Candidate.new(attributes)
     @candidates << new_can
     new_can
+  end
+
+  def open?
+    @open
+  end
+
+  def close!
+    @open = false
+  end
+
+  def winner
+    return false if open?
+    @candidates.max_by {|candidate| candidate.votes}
   end
 end
